255cc206a1b79549a64a0b1b2698edc4eac4b912
Duc DoBa <dobaduc@gmail.com>
2015-08-26 20:49:34 +0900

    Initial Commit

---
+++ SegmentedController/SegmentedController.h
@@ -0,0 +1,19 @@
+//
+//  SegmentedController.h
+//  SegmentedController
+//
+//  Created by Duc DoBa on 8/26/15.
+//  Copyright (c) 2015 Ducky Duke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SegmentedController.
+FOUNDATION_EXPORT double SegmentedControllerVersionNumber;
+
+//! Project version string for SegmentedController.
+FOUNDATION_EXPORT const unsigned char SegmentedControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SegmentedController/PublicHeader.h>
+
+
